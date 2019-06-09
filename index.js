b7aa56647495cf6a534d87cbd5d6131cf20056d3
SeonHyungJo <seonhyung.jo@gmail.com>
2019-06-09 23:37:37 +0900

    SeonHyungJo / PR시 커밋된 파일을 보고 자동으로 링크추가하는 기능 추가

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ module.exports = app => {
     return context.github.issues.createComment(issueComment)
   })
 
+  app.on('pull_request.opened', async context => {
+    const filesChanged = await context.github.pullRequests.getFiles(context.issue())
+    console.log("filesChanged", filesChanged)
+    const results = filesChanged.data.filter(file => file.filename.includes('.md'))
+    console.log("results", `${context.payload.pull_request.head.repo.html_url}/blob/${context.payload.pull_request.head.ref}`)
+    if (results && results.length > 0) {
+      // make URLs
+      let urls = ''
+      await results.forEach(async (result) => {
+        urls += `[${result.filename}](${context.payload.pull_request.head.repo.html_url}/blob/${context.payload.pull_request.head.ref}/${result.filename})`
+      })
+      await context.github.pullRequests.update(context.issue({body: `${context.payload.pull_request.body}\n${urls}`}))
+    }
+  })
+
+  // app.on('pull_request.edited', async context => {
+  // })
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
